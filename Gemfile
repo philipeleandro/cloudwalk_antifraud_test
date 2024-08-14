35bfea3f526a93ea30e5ff9596c4ea8a63939e3e
Philipe Leandro <philipe-leandro@hotmail.com>
2024-08-14 18:47:16 -0300

    Change database to sqlite

--- Gemfile
+++ Gemfile
@@ -10,8 +10,6 @@ gem "sprockets-rails"
 
 gem "sqlite3", "~> 1.4"
 
-gem 'pg'
-
 gem "puma", ">= 5.0"
 
 gem "importmap-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_01_195219) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
