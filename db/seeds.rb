5e7ca4f27b2b3ef02f37f77f76824b879a381394
Philipe Leandro <philipe-leandro@hotmail.com>
2024-08-17 12:25:32 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,37 @@
-# frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'factory_bot'
+
+# Create User
+puts "Creating User to manager app"
+User.create!(email: "cloudwalk_test@test.com", password: "password")
+puts "User created"
+puts "-------------------------------------------------------------"
+
+puts "Creating approve transaction data"
+# Create TransactionRisk data
+args = { transaction_id: "21320398",
+         merchant_id: "29744",
+         user_id: "97051",
+         card_number: "434505******9116",
+         transaction_date: "2019-12-01T23:16:32.812632",
+         transaction_amount: "374.56",
+         device_id: "285475",
+         has_cbk: false,
+         recommendation: "approve"}
+
+FactoryBot.create_list(:transaction_risk, 2, args)
+puts "TransactionRisk create"
+puts "-------------------------------------------------------------"
+
+puts "Creating deny transaction data"
+args = { transaction_id: "21320399",
+  merchant_id: "92895",
+  user_id: "2708",
+  card_number: "444456******4210",
+  transaction_date: "2019-12-01T22:45:37.873639",
+  transaction_amount: "734.87",
+  device_id: "497105",
+  has_cbk: true,
+  recommendation: "deny"}
+
+FactoryBot.create_list(:transaction_risk, 2, args)
+puts "TransactionRisk create"
